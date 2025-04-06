c1cfbd303b7a6d319377760249cbe56a2f7dc2c4
ManzzxVIP <manzdemox@gmail.com>
2025-04-07 06:40:40 +0700

    Update teksjpm.js

--- database/teksjpm.js
+++ database/teksjpm.js
@@ -1,6 +1,6 @@
-*XlesyVIP*
-*Creator Gxyenn*
-`Price harga Script XlesyVIP`
+*ManzzxVIP*
+*Creator Manzzx*
+`Price harga Script ManzzzxVIP`
 _15K ENC_
 _30K NO ENC_
 > Kelebihan Script
@@ -9,4 +9,4 @@ _Fitur Lumayan Ada 527_.
 _Dan Lainya_
 
 *_Silahkan Pm JIKALAU Minat_*
-_owner_ : https://wa.me/623877636168?text=Order_Script_Xlesy-VIP
+_owner_ : https://wa.me/623879090684?text=Order_Script_MANZZX-VIP
