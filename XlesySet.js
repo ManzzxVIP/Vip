536a55fd0bf8aa6df5ca9e3b94f5ba31dc67b1aa
ManzzxVIP <manzdemox@gmail.com>
2025-04-07 06:21:13 +0700

    Update XlesySet.js

--- XlesySet.js
+++ XlesySet.js
@@ -1,17 +1,17 @@
 // config.js
-const XlesySet = {
-    owner: ['6283877636168'],
-    packname: 'Xlesy',
-    author: 'Gxyenn',
-    botname: 'XlesyBot',
+const ManzBoTz = {
+    owner: '6283879090684'],
+    packname: 'ManzzX',
+    author: 'Manzzx',
+    botname: 'ManzBoTz',
     listprefix: ['.'],
     listv: ['❀','✿','⚘','々'],
-    number_bot: "", // Kalo pake panel bisa masukin nomor di sini, jika belum ambil session. Format: '628xx'
-    dana: ['083877636168'],
-    gopay: ['083877636168'],
+    number_bot: "6283879090684", // Kalo pake panel bisa masukin nomor di sini, jika belum ambil session. Format: '628xx'
+    dana: ['083879090684'],
+    gopay: ['083116078124'],
     ovo: ['kosong'],
-    pulsa: ['083877636168'],
+    pulsa: ['083116078124'],
     qris: ['kosong'],
 };
 
-module.exports = XlesySet
+module.exports = manzzx
